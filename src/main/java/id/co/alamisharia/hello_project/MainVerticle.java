1469aab63ca3cb5d57de25f8ab07dab6e5e433c4
Rizky Ramadhan <rizkyramadhanajjkh@gmail.com>
2021-05-17 23:02:09 +0700

    Update MainVerticle.java

--- src/main/java/id/co/alamisharia/hello_project/MainVerticle.java
+++ src/main/java/id/co/alamisharia/hello_project/MainVerticle.java
@@ -37,7 +37,7 @@ public class MainVerticle extends AbstractVerticle {
 
     JsonObject response = new JsonObject()
       .put("status", 200)
-      .put("messages", "Hello from ALAMI (v1.0)")
+      .put("messages", "Hello from ALAMI (v1.0)");
 
     context.response().putHeader("Content-Type", "application/json");
     context.response().end(response.encodePrettily());
